11615e2a9f403df58b5a063993137a58f9ebe72e
SHOGO AIBA <aibamattya@gmail.com>
2025-06-05 09:33:58 +0900

    add dog api

--- routes/notes.js
+++ routes/notes.js
@@ -5,8 +5,7 @@ var router = express.Router();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-//mongodb 接続情報を uriの後に入力すること！　mongodb+srv://saiba:<db_password>@test.qm7rph6.mongodb.net/?retryWrites=true&w=majority&appName=test
-const uri = "mongodb+srv://saiba:<db_password>@test.qm7rph6.mongodb.net/?retryWrites=true&w=majority&appName=test";
+const uri = "***";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
